0a5adcecbc3d01d1e86c7705cf4ea933f0520061
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-09-27 03:29:03 +0700

    Tong Cac chu So

---
+++ tongCacSo/src/TongChuSo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TongChuSo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        System.out.print("Input number: ");
+        num = scanner.nextInt();
+        System.out.println("Sum Digits = "+ sumDigits(num));
+
+    }
+
+    public static int sumDigits(int num){
+        int sum=0, num2=0;
+        while (true){
+            sum += num%10;
+            num = num/10;
+
+            if (num/10==0) {
+                sum += num%10;
+                num = num/10;
+                break;
+            }
+        }
+        return sum;
+    }
+}
